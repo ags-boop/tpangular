24d576049de80f95b37f0d9e0dc1fcb1619c3ebe
Agustin Testa <testaagustin8@gmail.com>
2021-05-10 19:44:21 -0300

    Tabla-done

--- AngularTP/src/app/Componentes/acceder-medios/acceder-medios.component.ts
+++ AngularTP/src/app/Componentes/acceder-medios/acceder-medios.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MedioServicesService } from 'src/app/Services/medio-services.service';
-// const _ = require("lodash"); 
 
 @Component({
   selector: 'app-acceder-medios',
@@ -12,12 +11,12 @@ export class AccederMediosComponent implements OnInit {
   receiveddata : any=[];
 
   constructor(private route:Router,private router: ActivatedRoute,private Medio: MedioServicesService) { 
-   this.receiveddata = this.Medio.getdata()
+   this.receiveddata = this.Medio.getdata().sort()
+   console.log(this.receiveddata)
    // let gfg = _.orderBy(this.receiveddata, ['patron', 'tipoPeriodo'], 
    // ['asc', 'desc']);
 
    // console.log("data",gfg)
-   
  }
 
   
@@ -25,7 +24,6 @@ export class AccederMediosComponent implements OnInit {
   
   ngOnInit(): void {
     this.CurrentUserInBar = this.router.snapshot.paramMap.get("user")!;
-  
   }
 
 
